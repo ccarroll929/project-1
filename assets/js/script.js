f1cd4630e5bcf07cf2ad552b8643b479c38187e8
ccarroll929 <146889005+ccarroll929@users.noreply.github.com>
2024-01-15 17:17:18 -0600

    Added a calorie calculator in js and html and added content for about me and contact info to footer and linked it to the nav bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,162 @@
+// Added a calorie calculator
+let dailyBtn = document.getElementById("open-daily");
+let dailyClose = document.getElementById("daily-close");
+let mealInputEl = document.getElementById("item-name");
+console.log(dailyClose);
+
+// Stores entered items to local storage, set to an empty array
+let items = JSON.parse(localStorage.getItem('items'))?JSON.parse(localStorage.getItem('items')): []
+
+// Event listeners for buttons to add and delete meals from the list
+document.getElementById("add-meal").addEventListener("click", addMeal);
+document.getElementById("delete-all").addEventListener("click", deleteAllMeals);
+
+let itemList = document.getElementById("item-list");
+
+function addMeal(event){
+    let calorieReturn = document.getElementById('item-name').value;
+  $.ajax({
+    method: 'GET', url: 'https://api.calorieninjas.com/v1/nutrition?query=' + calorieReturn,
+    async: false,
+    headers: {'X-Api-Key': 'qbo/h6wWQsSwBkJM+iPrUw==fmXwpQujp8zdjibt'},
+    contentType: 'application/json',
+    success: function(result) {
+      console.log(result.items[0].calories);
+      $("#item-calories").val(result.items[0].calories);
+    },
+    error: function ajaxError(jqXHR) {
+    console.error('Error: ', jqXHR.responseText);
+  }});
+    event.preventDefault();
+    mealName = document.getElementById("item-name").value;
+    mealCalories = document.getElementById("item-calories").value;
+    let item = {
+      "itemName": mealName,
+      "itemCalories": mealCalories
+    }
+   
+    StorageCtrl.storeItem(item);
+    displayItems();
+  }
+
+  function updateMeal(){
+    taskButtonHandler.addEventListener("click",updateMeal)
+    console.log("update")
+    }
+
+    function deleteAllMeals(){
+        let list = document.getElementById("item-list");
+        while(list.firstChild){
+        list.removeChild(list.firstChild);
+      }
+      items = []
+      localStorage.setItem('items', JSON.stringify(items));
+        }
+
+        // Displays list of user meal inputs 
+      function displayItems(){
+        items = [];
+        if (localStorage.getItem('items')){items = JSON.parse(localStorage.getItem('items'));}
+        let calories = 0;
+      itemList.innerHTML = ""
+        items.forEach((item)=>{
+          let li = document.createElement("li");
+          li.innerText = "Meal: " +  item.itemName + "; Calories: " + item.itemCalories;
+    
+          $('#item-calories').value = '';
+          let deletebtn = document.createElement("button");
+          deletebtn.value = item.itemName
+          deletebtn.innerText = "delete"
+          li.appendChild(deletebtn);
+          itemList.appendChild(li);
+          deletebtn.addEventListener("click",function(event){
+            event.preventDefault()
+            console.log(event.target.value)
+            let removedItems = items.filter(item => {
+              console.log(item)
+              if (event.target.value !== item.itemName){
+                return item
+              }
+            })
+            console.log(removedItems)
+            items = removedItems
+            localStorage.setItem('items', JSON.stringify(items));
+            displayItems()
+          })
+          calories = calories + parseInt(item.itemCalories);
+        })
+        console.log(typeof(calories));
+        totalCaloriesSpan = document.getElementById("total-calories");
+        totalCaloriesSpan.innerText = calories;
+      }
+
+
+      document.addEventListener("DOMContentLoaded", function() {
+        //
+        displayItems();
+      });
+
+      const StorageCtrl = (function () {
+        function storeItem(item) {
+            let items;
+            // Check if any data in local storage
+            if (localStorage.getItem('items') === null) {
+              items = [];
+              items.push(item);
+              localStorage.setItem('items', JSON.stringify(items));
+            } else {
+              items = JSON.parse(localStorage.getItem('items'));
+              items.push(item);
+              localStorage.setItem('items', JSON.stringify(items));
+            }
+          }
+          // Retrieve items from local storage
+         function getItemsFromStorage() {
+            let items;
+            if (localStorage.getItem('items') === null) {
+              items = []
+            } else {
+              items = JSON.parse(localStorage.getItem('items'));
+            }
+            return items;
+          }
+          //Deleting data from local storage
+         function deleteItemFromStorage() {
+            let item = ItemCtrl.findToDelete();
+            console.log(item);
+            let items = getItemsFromStorage();
+            items.forEach(x => {
+              if (x.id == item.id) {
+                items.splice(x.id, 1);
+              }
+            })
+            localStorage.setItem('items', JSON.stringify(items));
+          }
+          //Updating the new user data inputs in local storage
+          function updateItemFromStorage() {
+            let items = ItemCtrl.data.items;
+            localStorage.setItem('items', JSON.stringify(items));
+          }
+          //Clearing out all data from local storage if user clicks "delete all" button
+          function deleteAllFromStorage() {
+            localStorage.removeItem('items');
+            ItemCtrl.nullifyTotalCallories();
+          }
+          // Returning all of the items listed above to track in local storage.
+          return {
+            storeItem,
+            getItemsFromStorage,
+            deleteItemFromStorage,
+            updateItemFromStorage,
+            deleteAllFromStorage
+          }
+        })();
+
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const calorieData = []; // Array to store daily calorie entries
 
@@ -17,7 +176,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 calorieData[existingEntryIndex].intake = parseInt(inputField.value, 10);
             } else {
                 calorieData.push({ date, intake: parseInt(inputField.value, 10) });
-            }
+            } 
         });
 
         return inputField;
@@ -99,15 +258,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-$.ajax({
-    method: 'GET',
-    url: 'https://api.calorieninjas.com/v1/nutrition?query=' + query,
-    headers: { 'X-Api-Key': 'l+o7KdZtcRQhu+LatJADQQ==kodmdUBGdqyc6As9'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
+// $.ajax({
+//     method: 'GET',
+//     url: 'https://api.calorieninjas.com/v1/nutrition?query=' + query,
+//     headers: { 'X-Api-Key': 'l+o7KdZtcRQhu+LatJADQQ==kodmdUBGdqyc6As9'},
+//     contentType: 'application/json',
+//     success: function(result) {
+//         console.log(result);
+//     },
+//     error: function ajaxError(jqXHR) {
+//         console.error('Error: ', jqXHR.responseText);
+//     }
+// });
